281124f0b600fc885ed35a2d974ef33efed26d8c
Igor Moiseev <igor.moiseev1990@gmail.com>
2025-12-28 16:00:27 +0300

    refactoring_routers_components

--- app/api/routers/components.py
+++ app/api/routers/components.py
@@ -1,151 +1,72 @@
 import re
 from http import HTTPStatus
+from typing import Type, TypeVar
 
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
 from app.api.validators import BaseValidator, validate_name
-from app.core.db import get_async_session
-from app.models.cable import (Alumoflex, Cable, Color, Drennage, Marker, Plastic)
+from app.core.db import Base, get_async_session
+from app.models.cable import (Alumoflex, Color, Drennage, Marker, Plastic)
 from app.schemas.base_schema import (AlumoflexDB, DrennageDB, MarkerDB, PlasticDB, ColorDB,
                                      AlumoflexCreate, DrennageCreate, MarkerCreate, PlasticCreate, ColorCreate)
 
-components_router = APIRouter(prefix='/components', tags=['components'])
+
+ModelType = TypeVar("ModelType", bound=DeclarativeBase)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+ResponseSchemaType = TypeVar("ResponseSchemaType", bound=BaseModel)
 
 
-#========================================================================Alumoflex
-@components_router.get('/alumoflex',
-                       response_model=list[AlumoflexDB]
-                       )
-async def get_alumoflex(session: AsyncSession = Depends(get_async_session)):
-    alumoflex = await session.execute(select(Alumoflex))
-    return alumoflex.scalars().all()
+components_router = APIRouter(prefix='/components')
 
 
-@components_router.post('/alumoflex',
-                        response_model=AlumoflexDB
-                        )
-async def post_alumoflex(obj_in: AlumoflexCreate,
-                         session: AsyncSession = Depends(get_async_session)):
-    obj_in_data = obj_in.dict()
-    # await BaseValidator(Alumoflex, 'size').name_is_count(obj_in_data['size'],
-    #                                                      session)
-    alumoflex = Alumoflex(**obj_in_data)
-    session.add(alumoflex)
-    await session.commit()
-    await session.refresh(alumoflex)
-    return alumoflex
+def create_router(model_name: str,
+                  db_model: Type[ModelType],
+                  create_schema: Type[CreateSchemaType],
+                  response_schema: Type[ResponseSchemaType]
+                  ) -> APIRouter:
+    
+    router = APIRouter(prefix=f'/{model_name}', tags=[model_name])
+
+    @router.get('/', response_model=list[response_schema])
+    async def get_component(session: AsyncSession = Depends(get_async_session)):
+        component = await session.execute(select(db_model))
+        return component.scalars().all()
 
 
-#========================================================================Drennage
-@components_router.get('/drennage',
-                       response_model=list[DrennageDB])
-async def get_drennage(session: AsyncSession = Depends(get_async_session)):
-    drennage = await session.execute(select(Drennage))
-    return drennage.scalars().all()
+    @router.post('/', response_model=response_schema)
+    async def post_component(obj_in: CreateSchemaType,
+                             session: AsyncSession = Depends(get_async_session)):
+        obj_in_data = obj_in.dict()
+        new_component_obj = db_model(**obj_in_data)
+        session.add(new_component_obj)
+        await session.commit()
+        await session.refresh(new_component_obj)
+        return new_component_obj
+    
+    return router
 
 
-@components_router.post('/drennage',
-                        response_model=DrennageDB
-                        )
-async def create_drennage(obj_in: DrennageCreate,
-                          session: AsyncSession = Depends(get_async_session)):
-    obj_in_data = obj_in.dict()
-    validate_name(obj_in_data['name'])
-    # await BaseValidator(Drennage, 'name').name_is_count(obj_in_data['name'],
-    #                                                     session)
-    drennage = Drennage(**obj_in_data)
-    session.add(drennage)
-    await session.commit()
-    await session.refresh(drennage)
-    return drennage
-
-
-#========================================================================Plastic
-@components_router.get('/plastic',
-                       response_model=list[PlasticDB])
-async def get_plastic(session: AsyncSession = Depends(get_async_session)):
-    plastic = await session.execute(select(Plastic))
-    return plastic.scalars().all()
-
-
-@components_router.post('/plastic',
-                        response_model=PlasticDB
-                        )
-async def create_plastic(obj_in: PlasticCreate,
-                         session: AsyncSession = Depends(get_async_session)):
-    obj_in_data = obj_in.dict()
-    # await BaseValidator(Plastic, 'name').name_is_count(obj_in_data['name'],
-    #                                                    session)
-    plastic = Plastic(**obj_in_data)
-    session.add(plastic)
-    await session.commit()
-    await session.refresh(plastic)
-    return plastic
-
-
-#========================================================================Color
-@components_router.get('/color',
-                       response_model=list[ColorDB])
-async def get_color(session: AsyncSession = Depends(get_async_session)):
-    color = await session.execute(select(Color))
-    return color.scalars().all()
-
-
-@components_router.post('/color',
-                        response_model=ColorDB
-                        )
-async def create_color(obj_in: ColorCreate,
-                       session: AsyncSession = Depends(get_async_session)):
-    obj_in_data = obj_in.dict()
-    # await BaseValidator(Color, 'name').name_is_count(obj_in_data['name'],
-    #                                                  session)
-    color = Color(**obj_in_data)
-    session.add(color)
-    await session.commit()
-    await session.refresh(color)
-    return color
-
-
-#========================================================================Marker
-@components_router.get('/marker',
-                       response_model=list[MarkerDB])
-async def get_marker(session: AsyncSession = Depends(get_async_session)):
-    marker = await session.execute(select(Marker))
-    return marker.scalars().all()
-
-
-@components_router.post('/marker',
-                        response_model=MarkerDB)
-async def create_marker(obj_in: MarkerCreate,
-                        session: AsyncSession = Depends(get_async_session)):
-    obj_in_data = obj_in.dict()
-    # await BaseValidator(Marker, 'name').name_is_count(obj_in_data['name'], session)
-    marker = Marker(**obj_in_data)
-    session.add(marker)
-    await session.commit()
-    await session.refresh(marker)
-    return marker
-
-
-#========================================================================Metall
-#@components_router.get('/metall',
-#                       response_model=list[BaseDB])
-#async def get_metall(session: AsyncSession = Depends(get_async_session)):
-#    metall = await session.execute(select(Metall))
-#    return metall.scalars().all()
-
-
-#@components_router.post('/metall',
-#                        response_model=BaseDB)
-#async def create_metall(obj_in: BaseCreate,
-#                        session: AsyncSession = Depends(get_async_session)):
-#    obj_in_data = obj_in.dict()
-#    await BaseValidator(Metall, 'name').name_is_count(obj_in_data['name'],
-#                                                      session)
-#    new_metall = Metall(**obj_in_data)
-#    session.add(new_metall)
-#    await session.commit()
-#    await session.refresh(new_metall)
-#    return new_metall
+components_router.include_router(create_router(model_name='alumoflex',
+                                               db_model=Alumoflex,
+                                               create_schema=AlumoflexCreate,
+                                               response_schema=AlumoflexDB))
+components_router.include_router(create_router(model_name='drennage',
+                                               db_model=Drennage,
+                                               create_schema=DrennageCreate,
+                                               response_schema=DrennageDB))
+components_router.include_router(create_router(model_name='plastic',
+                                               db_model=Plastic,
+                                               create_schema=PlasticCreate,
+                                               response_schema=PlasticDB))
+components_router.include_router(create_router(model_name='color',
+                                               db_model=Color,
+                                               create_schema=ColorCreate,
+                                               response_schema=ColorDB))
+components_router.include_router(create_router(model_name='marker',
+                                               db_model=Marker,
+                                               create_schema=MarkerCreate,
+                                               response_schema=MarkerDB))
--- bot/bot.py
+++ bot/bot.py
@@ -1,135 +1,135 @@
-import aiohttp
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.fsm.context import FSMContext
-import asyncio
-from aiogram.filters import CommandStart
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-import requests
-from pprint import pprint
-import json
-import sys
-import os
-from dotenv import load_dotenv
+# import aiohttp
+# from aiogram import Bot, Dispatcher, types, F
+# from aiogram.fsm.context import FSMContext
+# import asyncio
+# from aiogram.filters import CommandStart
+# from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+# import requests
+# from pprint import pprint
+# import json
+# import sys
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
-
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-FASTAPI_URL = 'http://localhost:8000/api/components/alumoflex/'
-BOT_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/'
-
-# from aiogram.types import ReplyKeyboardRemove
-# await message.answer("Клавиатура удалена", reply_markup=ReplyKeyboardRemove())
+# load_dotenv()
 
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-# print(sys.stdlib_module_names)
+# BOT_TOKEN = os.getenv('BOT_TOKEN')
+# FASTAPI_URL = 'http://localhost:8000/api/components/alumoflex/'
+# BOT_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/'
 
-@dp.message(CommandStart())
-async def command_start(message: Message) -> None:
-    print(message)#.model_dump_json(indent=4, exclude_none=True))
-    await message.answer('Угадывай!')
+# # from aiogram.types import ReplyKeyboardRemove
+# # await message.answer("Клавиатура удалена", reply_markup=ReplyKeyboardRemove())
 
 
-
+# bot = Bot(token=BOT_TOKEN)
+# dp = Dispatcher()
+# # print(sys.stdlib_module_names)
 
 # @dp.message(CommandStart())
 # async def command_start(message: Message) -> None:
-#     pprint(dir(message))
-#     button1 = KeyboardButton(text='Изоляцию')
-#     button2 = KeyboardButton(text='Оболочку')
-#     keyboard = ReplyKeyboardMarkup(keyboard=[[button1, button2]],
-#                                    resize_keyboard=True)
-#     await message.answer("Что производите?", reply_markup=keyboard)
-
-
-# @dp.message((F.text == "Изоляцию") | (F.text == "Оболочку"))
-# async def select_constr(message: Message, state: FSMContext) -> None:
-#     await state.update_data(work_choice=message.text) # Сохраняем выбор Оболочку или Изоляцию
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get('http://127.0.0.1:8000/api/construction/') as response:
-#             if response.status == 200:
-#                 buttons = []
-#                 data = await response.json()
-#                 for i in data:
-#                     buttons.append(KeyboardButton(text=str(i['number'])))
-#                 keyboard = ReplyKeyboardMarkup(keyboard=[buttons], resize_keyboard=True)
-#     await message.answer("Выберите конструкцию", reply_markup=keyboard)
-
-
-# @dp.message()
-# async def process_construction(message: Message, state: FSMContext):
-#     user_data = await state.get_data()
-#     work_choice = user_data.get('work_choice')
-#     constr_choice = user_data.get('constr_choice')
-#     if work_choice not in ["Изоляцию", "Оболочку"]:
-#         await message.answer("Пожалуйста, сначала выберите Изоляцию или Оболочку.")
-#         return
-#     elif constr_choice is None and work_choice in ["Изоляцию", "Оболочку"]:
-#         await state.update_data(constr_choice=message.text)
-#         async with aiohttp.ClientSession() as session:
-#             async with session.get('http://127.0.0.1:8000/api/core/') as response:
-#                 if response.status == 200:
-#                     data = await response.json()
-#                     buttons = [KeyboardButton(text=str(i['name'])) for i in data]
-#                     keyboard = ReplyKeyboardMarkup(keyboard=[buttons], resize_keyboard=True)
-#                     await message.answer('Выберите жилу', reply_markup=keyboard)
-#                 else:
-#                     await message.answer("Ошибка получения данных с сервера.")
-#     elif constr_choice is not None:
-#         # выполнить запрос к апи cable с параметрами конструкции и жилы
-#         params = {'construction': int(constr_choice),
-#                   'core': message.text}
-#         async with aiohttp.ClientSession() as session:
-#             async with session.get('http://127.0.0.1:8000/api/cable/') as response:
-#                 if response.status == 200:
-#                     data = await response.json()
-#                     result_data = [item for item in data if item['construction']['number'] == int(constr_choice) and item['core']['name'] == message.text]
-#                     if work_choice == 'Изоляцию':
-#                         response_data = (f'Ваша изоляция:\n'
-#                                          f'Конструкция: K-{(result_data[0]['construction']['number'])} {result_data[0]['construction']['name']}\n'
-#                                          f'Жила: {result_data[0]['core']['name']}{result_data[0]['core']['metall']['name']}\n'
-#                                          f'Материал: сюда добавить материал изоляции\n'#{result_data[0]['construction']['plastic']['name']}\n'
-#                                          f'Толщина оболочки: {result_data[0]['thickness_isolat']}\n'
-#                                          f'Диаметр: {result_data[0]['diametr_isolate']}'
-#                                          )
-#                     if work_choice == 'Оболочку':
-#                         response_data = (f'Ваша оболочка:\n'
-#                                          f'Конструкция: K-{(result_data[0]['construction']['number'])} {result_data[0]['construction']['name']}\n'
-#                                          f'Жила: {result_data[0]['core_rel']['name']}\n'
-#                                          f'Материал: {result_data[0]['construction']['plastic']['name']}\n'
-#                                          f'Толщина оболочки: {result_data[0]['thickness_shell']}\n'
-#                                          f'Диаметр: {result_data[0]['diametr_shell']}'
-#                                          )
-
-#         await message.answer(response_data)
+#     print(message)#.model_dump_json(indent=4, exclude_none=True))
+#     await message.answer('Угадывай!')
 
 
 
 
+# # @dp.message(CommandStart())
+# # async def command_start(message: Message) -> None:
+# #     pprint(dir(message))
+# #     button1 = KeyboardButton(text='Изоляцию')
+# #     button2 = KeyboardButton(text='Оболочку')
+# #     keyboard = ReplyKeyboardMarkup(keyboard=[[button1, button2]],
+# #                                    resize_keyboard=True)
+# #     await message.answer("Что производите?", reply_markup=keyboard)
 
 
-    # await state.clear()
+# # @dp.message((F.text == "Изоляцию") | (F.text == "Оболочку"))
+# # async def select_constr(message: Message, state: FSMContext) -> None:
+# #     await state.update_data(work_choice=message.text) # Сохраняем выбор Оболочку или Изоляцию
+# #     async with aiohttp.ClientSession() as session:
+# #         async with session.get('http://127.0.0.1:8000/api/construction/') as response:
+# #             if response.status == 200:
+# #                 buttons = []
+# #                 data = await response.json()
+# #                 for i in data:
+# #                     buttons.append(KeyboardButton(text=str(i['number'])))
+# #                 keyboard = ReplyKeyboardMarkup(keyboard=[buttons], resize_keyboard=True)
+# #     await message.answer("Выберите конструкцию", reply_markup=keyboard)
 
 
-# @dp.message()
-# async def funk(message: Message, state: FSMContext):
+# # @dp.message()
+# # async def process_construction(message: Message, state: FSMContext):
+# #     user_data = await state.get_data()
+# #     work_choice = user_data.get('work_choice')
+# #     constr_choice = user_data.get('constr_choice')
+# #     if work_choice not in ["Изоляцию", "Оболочку"]:
+# #         await message.answer("Пожалуйста, сначала выберите Изоляцию или Оболочку.")
+# #         return
+# #     elif constr_choice is None and work_choice in ["Изоляцию", "Оболочку"]:
+# #         await state.update_data(constr_choice=message.text)
+# #         async with aiohttp.ClientSession() as session:
+# #             async with session.get('http://127.0.0.1:8000/api/core/') as response:
+# #                 if response.status == 200:
+# #                     data = await response.json()
+# #                     buttons = [KeyboardButton(text=str(i['name'])) for i in data]
+# #                     keyboard = ReplyKeyboardMarkup(keyboard=[buttons], resize_keyboard=True)
+# #                     await message.answer('Выберите жилу', reply_markup=keyboard)
+# #                 else:
+# #                     await message.answer("Ошибка получения данных с сервера.")
+# #     elif constr_choice is not None:
+# #         # выполнить запрос к апи cable с параметрами конструкции и жилы
+# #         params = {'construction': int(constr_choice),
+# #                   'core': message.text}
+# #         async with aiohttp.ClientSession() as session:
+# #             async with session.get('http://127.0.0.1:8000/api/cable/') as response:
+# #                 if response.status == 200:
+# #                     data = await response.json()
+# #                     result_data = [item for item in data if item['construction']['number'] == int(constr_choice) and item['core']['name'] == message.text]
+# #                     if work_choice == 'Изоляцию':
+# #                         response_data = (f'Ваша изоляция:\n'
+# #                                          f'Конструкция: K-{(result_data[0]['construction']['number'])} {result_data[0]['construction']['name']}\n'
+# #                                          f'Жила: {result_data[0]['core']['name']}{result_data[0]['core']['metall']['name']}\n'
+# #                                          f'Материал: сюда добавить материал изоляции\n'#{result_data[0]['construction']['plastic']['name']}\n'
+# #                                          f'Толщина оболочки: {result_data[0]['thickness_isolat']}\n'
+# #                                          f'Диаметр: {result_data[0]['diametr_isolate']}'
+# #                                          )
+# #                     if work_choice == 'Оболочку':
+# #                         response_data = (f'Ваша оболочка:\n'
+# #                                          f'Конструкция: K-{(result_data[0]['construction']['number'])} {result_data[0]['construction']['name']}\n'
+# #                                          f'Жила: {result_data[0]['core_rel']['name']}\n'
+# #                                          f'Материал: {result_data[0]['construction']['plastic']['name']}\n'
+# #                                          f'Толщина оболочки: {result_data[0]['thickness_shell']}\n'
+# #                                          f'Диаметр: {result_data[0]['diametr_shell']}'
+# #                                          )
+
+# #         await message.answer(response_data)
 
 
 
 
 
 
+#     # await state.clear()
+
+
+# # @dp.message()
+# # async def funk(message: Message, state: FSMContext):
 
 
 
 
 
 
-async def main() -> None:
-    await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+
+
+
+
+# async def main() -> None:
+#     await dp.start_polling(bot)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
